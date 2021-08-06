0acf2196d29f3b56e226a20e50c3eac60f5c4ca4
Moreno L. Telles <moreno.leoncio@gmail.com>
2021-08-06 04:13:39 -0300

    Teste Técnico - Honda Banzai

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+// GLIDER 
+window.addEventListener('load', function () {
+    new Glider(document.querySelector('.glider'), {
+        // Mobile-first defaults
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        scrollLock: true,
+        rewind: true,
+        draggable: true,
+        dots: '.dots',
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+        },
+        responsive: [{
+            // screens greater than >= 775px
+            breakpoint: 775,
+            settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+            }
+        }, {
+            // screens greater than >= 1024px
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+            }
+        }]
+    });
+})
